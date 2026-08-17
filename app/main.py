d386353c5aabad8419ab2ec12d7ef9faba1d9958
Abdul Wasay Tahir <abdulwasaytahir@gmail.com>
2026-08-17 16:19:32 +0500

    Add GET users endpoint

--- app/main.py
+++ app/main.py
@@ -6,3 +6,21 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "REST API is running"}
+
+
+@app.get("/users")
+def get_users():
+    return {
+        "users": [
+            {
+                "id": 1,
+                "name": "Ali",
+                "email": "ali@example.com"
+            },
+            {
+                "id": 2,
+                "name": "Ahmed",
+                "email": "ahmed@example.com"
+            }
+        ]
+    }
